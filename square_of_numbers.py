16bb140a0045d6683c2ea71eda0c781a633a8067
mudassirkhan19 <mudassirkhan19@users.noreply.github.com>
2017-05-28 05:54:20 +0000

    Done

--- square_of_numbers.py
+++ square_of_numbers.py
@@ -5,6 +5,5 @@ def squareOfNumbers(n):
 
     dict1 = {}
     for i in range(1, n+1):
-        dict1[i]=i**2
-            #dict1.update({i: i ** 2})
+        dict1.update({i: i ** 2})
     return dict1
