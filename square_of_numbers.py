afb7cc5880ff2690a03bd17b87ba64572531722a
RupaliHangekar <RupaliHangekar@users.noreply.github.com>
2017-06-01 03:56:39 +0000

    Done

--- square_of_numbers.py
+++ square_of_numbers.py
@@ -2,7 +2,12 @@ def squareOfNumbers(x):
     d={}
     if 0<x<100:
         for k in range(1,x+1):
-            d[k]=k**2
+            d[k]=k*k
         return d
-a = squareOfNumbers(3)
+    #if 0<x<100:
+    #    d = [k:k*k for k in range(1,x+1)]
+    #return d
+
+#n=input("Enter a number: ")
+a = squareOfNumbers(5)
 print a
