cb01000e9b1ca762958aedca055805e987083d04
nikhil-ds <prasadnikhil25@gmail.com>
2017-08-12 13:22:02 +0000

    Done

--- square_of_numbers.py
+++ square_of_numbers.py
@@ -1,7 +1,8 @@
 def squareOfNumbers(n):
-    squares = {}
-    for i in range(1, n+1):
+    D = {x:x**2 for x in range(1, n+1)}
+    return D
+    '''for i in range(1, n+1):
         squares[i] = i ** 2
-    return squares
-
+    return squares'''
+#print squareOfNumbers(8)
 #print type(number)
